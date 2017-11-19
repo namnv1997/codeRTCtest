854f169248ee30a0ed22059dba6759fbd1174751
namnv <helloimnvn@gmail.com>
2017-11-19 13:43:18 +0700

    aa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('express')();
-var http = require('https').Server(app);
-var io = require('socket.io')(https);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
